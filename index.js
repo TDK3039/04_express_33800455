7209faa6fe34abe4349b9cf90bff41d93d372f3a
Drajah223 <davidrajah325@outlook.com>
2025-10-23 17:05:51 +0100

    change "" to ' '

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 8000;
 
 // Load the route handlers
 const mainRoutes = require("./routes/main");
-app.use("/", mainRoutes);
+app.use('/', mainRoutes);
 
 // Start listening for HTTP requests
 app.listen(port, () => {
